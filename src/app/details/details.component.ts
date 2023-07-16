ee5a9e6c5754ee70f7999c2a387f394ea4b66606
Jonathan Phan <jtphan@asu.edu>
2023-07-16 15:27:33 -0700

    Complete routing for details component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
@@ -7,11 +10,21 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <p>
-      details works!
+      details works! {{housingLocationId}}
     </p>
   `,
   styleUrls: ['./details.component.css']
 })
+
+//DetailsComponent class
 export class DetailsComponent {
 
+  //Enables access to data about current route
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
+
+  //Convert id parameter from the route to a number
+  constructor(){
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
